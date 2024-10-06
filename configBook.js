8166eda46beda8dfdd3aa100083c4eb49fab6457
darkky9197 <darkking9197@gmail.com>
2024-10-06 12:33:33 +0530

    commityrf

--- configBook.js
+++ configBook.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const connect = mongoose.connect('mongodb+srv://darkking9197:179324865%40%40%24%23QPal@darkcluter9197.mr3j0ar.mongodb.net/Booknest');
+const connect = mongoose.connect('mongodb+srv://darkking9197:179324865%40%40%24%23QPal@darkcluter9197.mr3j0ar.mongodb.net/BookNest');
 
 connect.then(()=>{
-    console.log("Connected successfully to database - 2");
+    console.log("Connected successfully to database - 1");
 })
 .catch(()=>{
     console.log("Database cannot be connected")
